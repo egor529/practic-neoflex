071e400b49f1353b3984b1c8451d5967586be5ae
egor529 <wlist9998@gmail.com>
2025-11-20 17:34:13 +0400

    the first

--- Project/src/App.tsx
+++ Project/src/App.tsx
@@ -1,14 +1,34 @@
 import style from "./App.module.css"
 
 function App() {
- 
+
   return (
     <>
-      <nav>
-        <h1 className={style.header}> 
-          Портфолио
-        </h1>
-      </nav>
+    <div className={style.App}>
+    <nav className={style.navigation}>
+          <button className={style.menuButton}>Главная</button>
+          <button className={style.menuButton}>Услуги</button>
+          <button className={style.menuButton}>Контакты</button>
+        </nav>
+        <nav>
+          <h1 className={style.logo}></h1>
+          <h2 className={style.header}>Портфолио</h2>
+        </nav>
+        <div className={style.container}>
+          <h1>Егор Симонов</h1>
+        </div>
+        <div className={style.container}>
+          <p>Я — веб-разработчик с опытом работы в создании современных и эффективных веб-приложений. 
+            Моя страсть к программированию и дизайну помогает мне создавать интуитивно понятные и привлекательные интерфейсы,
+            которые радуют пользователей.</p>
+        </div>
+        <button className={style.button}>Узнать подробнее</button>
+        <section className={style.aboutMe}>
+          <h2>Обо мне</h2>
+          <p>Меня зовут Егор Симонов, я опытный веб-разработчик, занимаюсь созданием красивых и функциональных приложений.
+            Люблю экспериментировать с новыми технологиями и постоянно совершенствуюсь.</p>
+        </section>
+        </div>
     </>
   )
 }
